1723373b3457c707909299cf1081793e58487311
Bothina-cs3 <bothinamohammed944@gmail.com>
2024-02-18 23:55:00 +0300

    0x1B. C - Sorting algorithms & Big O

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,30 +9,30 @@
 
 void selection_sort(int *array, size_t size)
 {
-        int tmp;
-        size_t i, j, index;
+	int tmp;
+	size_t i, j, index;
 
-        if (!array || size < 2)
-            return;
+	if (!array || size < 2)
+		return;
 
-        i = 0;
-        while (i < size - 1)
-        {
-            index = i;
-            for (j = i + 1; j < size; j++)
-            {
-                if(array[j] < array[index])
-                        index = j;
-            }
+	i = 0;
+	while (i < size - 1)
+	{
+		index = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[index])
+				index = j;
+		}
 
-            if (index != i)
-            {
-                tmp = array[i];
-                array[i] = array[index];
-                array[index] = tmp;
-                 print_array(array, size);
-            }
+		if (index != i)
+		{
+			tmp = array[i];
+			array[i] = array[index];
+			array[index] = tmp;
+			print_array(array, size);
+		}
 
-            i++;
-            }
+		i++;
+	}
 }
